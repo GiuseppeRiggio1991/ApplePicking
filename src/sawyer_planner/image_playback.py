f02fe80d3666b4d817c7ee51886514d4b9e24c69
Alexander You <youa@oregonstate.edu>
2019-12-16 16:59:42 -0800

    Added util file for playing back recorded file topics

---
+++ src/sawyer_planner/image_playback.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import rospy
+import cPickle
+import os
+
+last_file = ''
+last_obj = {}
+data_loc = os.path.join(os.path.expanduser('~'), 'data', 'tree_data')
+
+publishers = {}
+
+rospy.init_node('image_playback')
+
+while not rospy.is_shutdown():
+
+    now = rospy.Time.now()
+    if last_obj:
+
+        for k, v in last_obj.iteritems():
+            if k not in publishers:
+                publishers[k] = rospy.Publisher(k, type(v), queue_size=1)
+            v.header.stamp = now
+            publishers[k].publish(v)
+
+    new_file = rospy.get_param('playback_file', '')
+
+    if new_file != last_file:
+        last_file = new_file
+        file_name = os.path.join(data_loc, '{}.pickle'.format(new_file.replace('.pickle', '')))
+        try:
+            with open(file_name, 'rb') as fh:
+                last_obj = cPickle.load(fh)
+            rospy.loginfo('Loaded file {}!'.format(new_file))
+        except IOError:
+            rospy.logerr('No file named {}!'.format(new_file))
+
+    rospy.sleep(0.1)
